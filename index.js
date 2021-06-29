db57b107881e74066f20b1b7159bcf314248fa88
TanushriChatterjee <tanushri1595@gmail.com>
2021-06-29 23:34:30 +0530

    task completed

--- index.js
+++ index.js
@@ -150,6 +150,11 @@ const saveEditableChanges=(event)=>{
     return task;
   });
   localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+  taskTitle.setAttribute("contenteditable","false");
+  taskDescription.setAttribute("contenteditable","false");
+  taskType.setAttribute("contenteditable","false");
+  submitButton.removeAttribute("onclick");
+  submitButton.innerHTML="Open Task";
 };
 
 
